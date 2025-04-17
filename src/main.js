b4444617ad6ff0b7809ddbb21e9649384dba61a9
naowmi <125772469+naowmi@users.noreply.github.com>
2025-04-17 21:24:04 +0200

    up6

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-const button = document.querySelector('.mobile-menu-open-icon');
-const modalIcon = document.querySelector('.mobile-menu');
-const closeBtnMenu = document.querySelector('.mobile-menu-btn-close')
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.mobile-menu-open-icon');
+    const modalIcon = document.querySelector('.mobile-menu');
+    const closeBtnMenu = document.querySelector('.mobile-menu-btn-close');
 
-button.addEventListener('click', () => {
-    modalIcon.classList.add('is-open');
-});
+    if (button && modalIcon && closeBtnMenu) {
+        button.addEventListener('click', () => {
+            modalIcon.classList.add('is-open');
+        });
 
-closeBtnMenu.addEventListener('click', () => {
-    modalIcon.classList.remove('is-open')
+        closeBtnMenu.addEventListener('click', () => {
+            modalIcon.classList.remove('is-open');
+        });
+    } else {
+        console.warn('Один з елементів не знайдено в DOM.');
+    }
 });
