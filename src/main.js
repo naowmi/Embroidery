50cf28f870c46ccfc7cd83fc589c93eb8ec512e1
naowmi <125772469+naowmi@users.noreply.github.com>
2025-05-07 22:51:34 +0200

    up11

--- src/main.js
+++ src/main.js
@@ -9,3 +9,30 @@ button.addEventListener('click', () => {
 closeBtnMenu.addEventListener('click', () => {
     modalIcon.classList.remove('is-open')
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.order-button');
+    const select = document.getElementById('product-select');
+
+    buttons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault(); 
+
+        const product = button.getAttribute('data-product');
+
+        if (select) {
+          for (const option of select.options) {
+            if (option.value === product) {
+              select.value = product;
+              break;
+            }
+          }
+
+          select.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+          select.focus();
+        }
+      });
+    });
+  });
